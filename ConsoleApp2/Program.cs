cc7ce34a3c3c3e19a2b2a71eec5c3c4a3a840d81
Trinadh45 <gancheerithrinadharao@gmail.com>
2026-08-29 23:16:10 +0530

    Revert "my sub branch"
    This reverts commit da626c21d5e2d75d9be39ab1d58e1caf199e150f.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,15 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
-namespace ConsoleApp2
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to the C# Console Application!");
-            Console.WriteLine("Hello");
-        }
-    }
-}
-
+Console.WriteLine("Welcome to the C# Console Application!");
+Console.WriteLine("Welcome to the C# Console Application!");
